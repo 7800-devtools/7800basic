11474b32ad6f78108553c32d31487fcf474ab34c
msaarna@gmail.com <msaarna@gmail.com>
2020-03-04 20:34:42 -0500

    real for...next step -1 fix

--- 7800bas.c
+++ 7800bas.c
@@ -11,7 +11,7 @@
 char stdoutfilename[256];
 FILE *stdoutfilepointer;
 
-#define BASIC_VERSION_INFO "7800basic v0.8-RC2"
+#define BASIC_VERSION_INFO "7800basic v0.8-RC3"
 
 int main(int argc, char *argv[])
 {
