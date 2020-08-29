5fa01a2f819dd12b699b5f6169fe7d94737029b4
msaarna@gmail.com <msaarna@gmail.com>
2020-08-29 09:07:51 -0400

    uprev to v0.11

--- 7800bas.c
+++ 7800bas.c
@@ -13,7 +13,7 @@ FILE *stdoutfilepointer;
 
 extern int currentdmahole;
 
-#define BASIC_VERSION_INFO "7800basic v0.11-a8"
+#define BASIC_VERSION_INFO "7800basic v0.11"
 
 int main(int argc, char *argv[])
 {
