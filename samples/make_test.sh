24a489ac53b74871703bf9e66df12eb8e2610f73
msaarna@gmail.com <msaarna@gmail.com>
2020-08-31 08:45:22 -0400

    add pokey sample to build test

--- samples/make_test.sh
+++ samples/make_test.sh
@@ -4,7 +4,7 @@ echo "    [$(7800basic.sh -v)]"
 make all >/dev/null 2>&1
 for FILE in $(cat sizes.ref | awk '{print $1}') ; do
         RECORDEDSIZE=$(grep "$FILE" sizes.ref | awk '{print $2}')
-        REALSIZE=$(du -sb "$FILE"/"$FILE".bas.a78 | awk '{print $1}')
+        REALSIZE=$(du -sb "$FILE"/"$FILE".bas.a78 2>/dev/null | awk '{print $1}')
         if [ "$RECORDEDSIZE" = "$REALSIZE" ] ; then
                 echo "    "PASS: $FILE
         else
