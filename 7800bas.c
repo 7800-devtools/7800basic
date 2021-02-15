ded75a514c539d2b9f780f902ff26988b89f62ab
msaarna@gmail.com <msaarna@gmail.com>
2021-02-14 21:38:51 -0500

    uprev for release

--- 7800bas.c
+++ 7800bas.c
@@ -13,7 +13,7 @@ FILE *stdoutfilepointer;
 
 extern int currentdmahole;
 
-#define BASIC_VERSION_INFO "7800basic v0.17-SNAPSHOT "
+#define BASIC_VERSION_INFO "7800basic v0.17"
 
 int main(int argc, char *argv[])
 {
