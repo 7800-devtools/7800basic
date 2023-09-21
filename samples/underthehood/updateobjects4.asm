0aec14fbfe564eea82e77f2c5d4dc8634d574c3f
msaarna@gmail.com <msaarna@gmail.com>
2023-09-20 20:00:30 -0400

    update for 4 byte objects

--- samples/underthehood/updateobjects4.asm
+++ samples/underthehood/updateobjects4.asm
@@ -1,7 +1,7 @@
 ; --------------------------------------------------------------------------
-; UpdateObjects assembly code provided by RevEng (Mike Saarna)
-; (only works with default sprites in 7800basic - if you're using the  
-; 4 byte sprites, use the UpdateObjects4 assembly file instead.)   
+; UpdateObjects4 assembly code provided by RevEng (Mike Saarna)
+; (Use this assembly file if you're using 4 byte sprites. If not, then use)
+; the UpdateObjects assembly file instead.)   
 ;   
 ;   allows enabling, disabling, setting palette, or graphic for any object
 ;   in the display lists, given the zone number (Y) and the number of the 
@@ -12,7 +12,7 @@
 ; EnableObject.  Addition of SetObjectX and SetObjectY macros.
 ;   
 ; updated 20200301 - Update to support full extended screen height, and up
-; to 40 objects in the zone. Extend the x5table if you need more objects, 
+; to 40 objects in the zone. Extend the x4table if you need more objects, 
 ; but really, Maria won't be able to likely draw more than 40.
 ; -------------------------------------------------------------------------- 
 
@@ -50,7 +50,7 @@
  MAC SetObjectPalette
   ; SetObjectPalette - X=object_X Y=object_Y {1}=palette
  jsr SetObjectPointer
- ldy #3 ; palette+width is the 4th byte in the dl object
+ ldy #1 ; palette+width is the 2nd byte in the dl object
  lda (temp1),y
  and #%00011111
  ldx {1}
@@ -62,7 +62,7 @@
   MAC SetObjectX
   ; SetObjectX - X=object_X Y=object_Y {1}=X-coordinate
  jsr SetObjectPointer
- ldy #4 ; object x-coordinate is the 5th byte in the dl object
+ ldy #3 ; object x-coordinate is the 4th byte in the dl object
  lda {1}
  sta (temp1),y
  ENDM
@@ -111,18 +111,18 @@
 SetObjectPointer ; called with X=object_X Y=object_Y
  clc
  lda ZONESTARTLO,y
- adc x5table,x
+ adc x4table,x
  sta temp1 
  lda ZONESTARTHI,y
  adc #0
  sta temp2
  rts
 
-x5table ; enough entries to support 40 objects per dl
-MULT5IDX SET 0
+x4table ; enough entries to support 40 objects per dl
+MULT4IDX SET 0
  REPEAT 40
- .byte MULT5IDX
-MULT5IDX SET (MULT5IDX+5)
+ .byte MULT4IDX
+MULT4IDX SET (MULT4IDX+4)
  REPEND
 
 x32table ; 32 multiplaction for palette values
