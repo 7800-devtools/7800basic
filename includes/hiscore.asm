594b98852403f43011d871409808ee4cfdec6819
msaarna@gmail.com <msaarna@gmail.com>
2024-05-05 11:24:17 -0400

    fix hi-score init when cart is all-zero

--- includes/hiscore.asm
+++ includes/hiscore.asm
@@ -44,15 +44,18 @@ detecthsc
 checkhscinit
              and $1000,y
              dey
-             bpl checkhscinit
+             bne checkhscinit
              cmp #$ff
-             bne hscisalreadyinit
+             beq dominimalhscinit
+             ; Y is 0
+             lda #0
 checkhscinit2
              ora $1000,y
              dey
-             bpl checkhscinit2
+             bne checkhscinit2
              cmp #0
              bne hscisalreadyinit
+dominimalhscinit
              ; if we're here, we need to do a minimal HSC init...
              ldy #$28
 hscinitloop1
