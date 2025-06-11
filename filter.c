6658a87fb481a20e7cd1fcbfa6e61a4745031f93
msaarna@gmail.com <msaarna@gmail.com>
2025-06-11 18:41:27 -0400

    strip out dasm generic undefined symbol output. (improved dasm to give specific symbols that held up assembly)

--- filter.c
+++ filter.c
@@ -2,7 +2,11 @@
 
 /*
      7800filter
-	reads from stdin, filters out a bunch of 7800basic symbol names, and writes to stdout.
+	filters out the huge dasm "Unresolved Symbols" list, which is useless
+	for us since all of the optional language features own several
+	harmless unresolved symbols.
+	We now use a dasm that complains about the exact symbols that caused
+	the assembly to be unresolvable.
 */
 
 #define BUFSIZE 1000
@@ -12,193 +16,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Our term list. If the term starts with ^ it will only be matched at the start
-// of a line...
-
-char *filterterm[] = {
-    "^bankswitchmode",
-    "^pauseroutineoff",
-    "^BANKRAM",
-    "^DEV",
-    "^PAL",
-    "^TIASFXMONO",
-    "^MCPDEVCART",
-    "^MULTIBUTTON",
-    "^debugcolor",
-    "^collisionwrap",
-    "^pokeysupport",
-    "^pokeysfxsupport",
-    "^pokeykeysupport",
-    "^ROM32K",
-    "^ROM52K",
-    "^ROM128K",
-    "^ROM256K",
-    "^ROM512K",
-    "^ROM272K",
-    "^ROM144K",
-    "^ROM528K",
-    "^USED_PLOTVALUEEXTRA",
-    "^DOUBLEWIDE ",
-    "^ZONEHEIGHT ",
-    "^BOXCOLLISION ",
-    "^ROM48K ",
-    "^ROM16K ",
-    "^ROM8K ",
-    "^ROMAT4K ",
-    "^CANARYOFF ",
-    "^EXTRADLMEMORY ",
-    "^USED_PLOTVALUE ",
-    "^LONGCONTROLLERREAD ",
-    "^DRIVINGSUPPORT ",
-    "^DRIVINGBOOST ",
-    "^KEYPADSUPPORT ",
-    "^MOUSESUPPORT ",
-    "^MOUSETIME ",
-    "^MOUSE0SUPPORT ",
-    "^MOUSE1SUPPORT ",
-    "^MOUSEXONLY ",
-    "^PRECISIONMOUSING ",
-    "^PADDLESUPPORT ",
-    "^PADDLE0SUPPORT ",
-    "^PADDLE1SUPPORT ",
-    "^PADDLESMOOTHINGOFF ",
-    "^PADDLESCALEX2 ",
-    "^ZONE ",
-    "^TWOPADDLESUPPORT ",
-    "^TRAKBALLSUPPORT ",
-    "^TRAKBALL0SUPPORT ",
-    "^TRAKBALL1SUPPORT ",
-    "^TRAKXONLY ",
-    "^TRAKTIME ",
-    "^TIAVOLUME ",
-    "^RMTVOLUME ",
-    "^RMTOFFSPEED ",
-    "^RMTNTSCSPEED ",
-    "^RMTPALSPEED ",
-    "^RMT ",
-    "^FOURBITFADE ",
-    "^PADDLERANGE ",
-    "^LIGHTGUNSUPPORT ",
-    "^DOUBLEBUFFER ",
-    "^PLOTVALUEPAGE ",
-    "^USED_ADJUSTVISIBLE ",
-    "^HSCHARSHERE ",
-    "^BANKSET",
-    "^isBANKSETBANK ",
-    "^SNES",
-    "^MEGA7800",
-    "^0.pause",
-    "^0.userinterrupt",
-    "^0.topscreenroutine",
-    "^0.bottomscreenroutine",
-    "^0.altgamestart",
-    "^0.HSup",
-    "^0.calledfunction_mul8",
-    "^0.calledfunction_mul16",
-    "^0.calledfunction_div8",
-    "^0.calledfunction_div16",
-    "^0.calledfunction_converttobcd",
-    "^interrupthold",
-    "_main2 ",
-    "_main3 ",
-    "_main4 ",
-    "^ATOMICSPRITEUPDATE",
-    "^FINESCROLLENABLED",
-    "^AVOXVOICE",
-    "^FRAMESKIPGLITCHFIX",
-    "^plotvalueonscreen",
-    "^included.pokeysound.asm",
-    "^included.rmtplayer.asm",
-    "^included.fourbitfade.asm",
-    "^included.7800vox.asm",
-    "^included.hiscore.asm",
-    "^included.mega7800.asm",
-    "^included.snes2atari.asm",
-    "^MUSICTRACKER",
-    "^HSSCORESIZE",
-    "^songdatastart_song_highscore",
-    "^sfx_highscore",
-    "^vox_highscore",
-    "^FRAMESKIPGLITCHFIXWEAK",
-    "^ZONECOUNT",
-    "^ZONEFROM ",
-    "^ZONETO ",
-    "^HSSECONDS",
-    "^DEBUGCOLOR",
-    "^DEBUGWAITCOLOR",
-    "^TURNEDOFF",
-    "^HSGAMENAMELEN",
-    "^HSSUPPORT",
-    "^CHECKOVERWRITE",
-    "^included.tracker.asm",
-    "^SCREENHEIGHT",
-    "^ZONE",
-    "^DEBUGINTERRUPT",
-    "^TIASFXMONO",
-    "^SPRITECOUNTING",
-    "^HSCOLORCHASESTART",
-    "^BUZZBASS",
-    "^NOTIALOCK",
-    "^hiscorefont",
-    "^MCPDEVCART",
-    "^HSCUSTOMLEVELNAMES",
-    "^HSNOLEVELNAMES",
-    "^DEBUGFRAMES",
-    "^HSGAMERANKS",
-    "^ONEBUTTONMODE",
-    "^SOFTRESETASPAUSEOFF",
-    "^DLMEMSTART",
-    "^DLMEMEND",
-    "^pokeysound.asm",
-    "^pokeyaddress",
-    "^rmtplayer.asm",
-    "^fourbitfade.asm",
-    "^BEADHEADER",
-    "^LONGDEBUG",
-    "^hiscore.asm",
-    "^tracker.asm",
-    "^mega7800.asm",
-    "^snes2atari.asm",
-    " Unresolved Symbols",
-    "^SGRAM",
-    "^ALWAYSTERMINATE",
-    "^NODRAWWAIT",
-    "^NOLIMITCHECKING",
-    "^INFO: Label",
-    "^CRASHDUMP",
-    "^BREAKPROTECT",
-    "^dumpbankswitch",
-    ""
-};
-
 int main (int argc, char **argv)
 {
     char linebuffer[BUFSIZE];
-    int t, match;
+    int t, match = 0;
     while (fgets (linebuffer, BUFSIZE, stdin) != NULL)
     {
-	match = 0;
-	for (t = 0; filterterm[t][0] != '\0'; t++)
+        if (strncmp(linebuffer,"--- Unresolved Symbol List", 26) == 0)
 	{
-	    if (filterterm[t][0] == '^')
-	    {
-		if (strncmp (linebuffer, filterterm[t] + 1, strlen (filterterm[t] + 1)) == 0)
-		{
-		    match = 1;
-		    break;
-		}
-	    }
-	    else
-	    {
-		if (strstr (linebuffer, filterterm[t]) != NULL)
-		{
-		    match = 1;
-		    break;
-		}
-	    }
+    		match = 1;
+		continue;
 	}
-	if (match == 0)
-	    fputs (linebuffer, stdout);
+        if ((strncmp(linebuffer,"--- ", 4) == 0) && (strstr(linebuffer,"Unresolved Symbols")!=NULL))
+	{
+    		match = 0;
+		continue;
+	}
+	if (match == 1)
+		continue;
+	fputs (linebuffer, stdout);
     }
     return (0);
 }
